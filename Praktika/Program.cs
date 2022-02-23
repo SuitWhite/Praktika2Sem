cb38a11d4b8e18a13d930b8bb5a1275ac808d089
Ihor Vasylets <44103737+SuitWhite@users.noreply.github.com>
2022-02-23 23:51:54 +0200

    !!

--- Praktika/Program.cs
+++ Praktika/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+
+using System.Reflection;
+using System.Reflection.Metadata;
 
 namespace Praktika
 {
